58ffa2fd8ec6a7455645891833760aee265a1d76
SND\pmiller_cp <SND\pmiller_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2009-10-10 21:15:00 +0000

    Proof of concept - demo setting is loaded from hard-coded value in provider
    git-svn-id: https://s3toconfig.svn.codeplex.com/svn@28663 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- Natol.S3ToConfig.ProtectedConfiguration/ProtectedConfigurationProvider.cs
+++ Natol.S3ToConfig.ProtectedConfiguration/ProtectedConfigurationProvider.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace Natol.S3ToConfig.ProtectedConfiguration
 {
-    public class ProtectedConfigurationProvider
+    public class ProtectedConfigurationProvider : System.Configuration.ProtectedConfigurationProvider
     {
 
+        public override System.Xml.XmlNode Decrypt(System.Xml.XmlNode encryptedNode)
+        {
+            string xmlRaw =
+                "<sampleConfig><settings sampleConfigSetting=\"Sucess. This Setting came from code.\"></settings></sampleConfig>";
+
+            var doc = new XmlDocument();
+            doc.LoadXml(xmlRaw);
+            return doc.ChildNodes[0];
+        }
+
+        public override System.Xml.XmlNode Encrypt(System.Xml.XmlNode node)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
