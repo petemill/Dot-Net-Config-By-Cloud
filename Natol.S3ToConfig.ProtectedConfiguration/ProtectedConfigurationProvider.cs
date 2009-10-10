7b6d21696a64c96511f04ccd74ed9fd87a40b55e
SND\pmiller_cp <SND\pmiller_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2009-10-10 22:29:50 +0000

    First fully functional version
    git-svn-id: https://s3toconfig.svn.codeplex.com/svn@28675 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

--- Natol.S3ToConfig.ProtectedConfiguration/ProtectedConfigurationProvider.cs
+++ Natol.S3ToConfig.ProtectedConfiguration/ProtectedConfigurationProvider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml;
+using LitS3;
 
 namespace Natol.S3ToConfig.ProtectedConfiguration
 {
@@ -11,16 +12,49 @@ namespace Natol.S3ToConfig.ProtectedConfiguration
 
         public override System.Xml.XmlNode Decrypt(System.Xml.XmlNode encryptedNode)
         {
-            string xmlRaw =
-                "<sampleConfig><settings sampleConfigSetting=\"Sucess. This Setting came from code.\"></settings></sampleConfig>";
+            //note: in order to verify the protected configuration route without going via s3, use the following line
+            //string xmlRaw = "<sampleConfig><settings sampleConfigSetting=\"Sucess. This Setting came from code.\"></settings></sampleConfig>";
+            
+            //setup parameters we need to know (note: this could be extended to be further provider-based)
+            string awsAccessKey, awsSecretKey, bucketName, objectKey;
 
+            //collect parameter values
+            XmlNode settingsNode = encryptedNode.SelectSingleNode("/EncryptedData/s3ProviderInfo");
+            awsAccessKey = settingsNode.Attributes["s3AccessKey"].Value;
+            awsSecretKey = settingsNode.Attributes["s3SecretKey"].Value;
+            bucketName = settingsNode.Attributes["s3BucketName"].Value;
+            objectKey = settingsNode.Attributes["objectKey"].Value;
+
+            //get value from s3
+            var service = new S3Service
+            {
+                AccessKeyID = awsAccessKey,
+                SecretAccessKey = awsSecretKey,
+                UseSsl = true,
+                UseSubdomains = true
+            };
+            string xmlRaw = service.GetObjectString(bucketName, objectKey);
+            
+            //cast to XmlDocument
             var doc = new XmlDocument();
             doc.LoadXml(xmlRaw);
+
+            //return node
             return doc.ChildNodes[0];
         }
 
+
+        /// <summary>
+        /// This method is not implemented due to the neccessary inclusion of parameters (eg: where to store the information), which we would not have access to
+        /// </summary>
+        /// <param name="node"></param>
+        /// <returns></returns>
         public override System.Xml.XmlNode Encrypt(System.Xml.XmlNode node)
         {
+            //note: this is not implemented because we would not know where to store the info
+            //however, this could be implemented based on it's own configured parameters in it's own
+            //configuration section, but we would still have the problem of not knowing which key of s3 settings
+            //we belonged to. Thankfully, self-writing back to app-configuration (instead of user-configuration) is not something that is often done.
             throw new NotImplementedException();
         }
     }
--- Natol.S3ToConfig.SampleConsoleApplication/Program.cs
+++ Natol.S3ToConfig.SampleConsoleApplication/Program.cs
@@ -10,9 +10,93 @@ namespace Natol.S3ToConfig.SampleConsoleApplication
     {
         static void Main(string[] args)
         {
+            //NOTE: before running this sample, you must replace the values in the DemoConfigurationFile
+            //with your s3 access details. You must also copy the contents of the configuration
+            //to the location you have chosen in s3.
+
+            //show user some command guidelines
+            WriteLineBreak(); WriteLineBreak();
+            Console.WriteLine("s3ToConfig Sample");
+            Console.WriteLine("NOTE: before running this sample, you must replace the values in the DemoConfigurationFile with your s3 access details. You must also copy the contents of the configuration to the location you have chosen in s3.");
+            
+            
+
+            //parse input from user until asked to quit
+            bool keepLooping = true;
+            while (keepLooping)
+            {
+                WriteLineBreak();
+                Console.WriteLine("Commands (press Enter to exit):");
+                ListCommands();
+                Console.Write("Enter a command: ");
+                string usrInput = Console.ReadLine().Trim();
+                if (String.IsNullOrEmpty(usrInput))
+                    keepLooping=false;
+                else if (usrInput.ToLower()=="refreshconfig")
+                {
+                    SampleConfigSection.RefreshConfig();
+                    Console.WriteLine("Config refreshed");
+                }
+                else if (usrInput.ToLower()=="readvalue")
+                {
+                    Console.WriteLine(String.Concat("Demo Setting Value: ",SampleConfigSection.Current.Settings.SampleConfigSetting));
+                }
+            }
+
             string demoSettingValueFound = SampleConfigSection.Current.Settings.SampleConfigSetting;
             Console.WriteLine(demoSettingValueFound);
             Console.ReadLine();
         }
+
+        #region Console Helper Methods
+
+        public static void WriteLineBreak()
+        {
+            Console.WriteLine("-----------------------------------------------------");
+        }
+        public static void ListCommands()
+        {
+            string[] commandList =
+                {
+                    "readvalue","refreshconfig"
+                };
+            foreach (string command in commandList)
+            {
+                Console.WriteLine(String.Concat(" -", command));
+            }
+        }
+        public static bool GetDebugOptions()
+        {
+            return AskYesNoQuestion("Write out status of each action?");
+        }
+        public static int AskForInteger(string message)
+        {
+            int tryParse;
+            if (Int32.TryParse(AskForInfo(message), out tryParse))
+            {
+                return tryParse;
+            }
+            else
+            {
+                Console.WriteLine("Error parsing text. Please enter a valid Integer.");
+                //recursive
+                return AskForInteger(message);
+            }
+        }
+        public static String AskForInfo(string message)
+        {
+            Console.WriteLine();
+            Console.Write(message);
+            string result = Console.ReadLine();
+            return result;
+        }
+        public static bool AskYesNoQuestion(string message)
+        {
+            Console.Write(String.Concat(message, " Press Y/N:"));
+            ConsoleKeyInfo key = Console.ReadKey(false);
+            Console.WriteLine(); return (key.Key == ConsoleKey.Y);
+
+        }
+        #endregion Console Helper Methods
     }
 }
