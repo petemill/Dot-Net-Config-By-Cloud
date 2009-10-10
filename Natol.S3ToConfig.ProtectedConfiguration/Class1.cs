24f553f778774c704c9a2fd0954a750263c70318
SND\pmiller_cp <SND\pmiller_cp@0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5>
2009-10-10 18:56:52 +0000

    Initial Checkin of VS projects/solution
    git-svn-id: https://s3toconfig.svn.codeplex.com/svn@28644 0e2699bc-83d4-4a8f-98e7-55e24ab8c7a5

---
+++ Natol.S3ToConfig.ProtectedConfiguration/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Natol.S3ToConfig.ProtectedConfiguration
+{
+    public class Class1
+    {
+    }
+}
